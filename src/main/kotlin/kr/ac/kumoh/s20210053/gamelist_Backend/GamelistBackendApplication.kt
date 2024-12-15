aca894c129aeb049aa63729fafb868778476ab47
kwak <knh239@naver.com>
2024-12-15 17:22:40 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210053/gamelist_Backend/GamelistBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210053.gamelist_Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamelistBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamelistBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210053/gamelist_Backend/GamelistBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210053.gamelist_Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamelistBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
